86f3e8964cddf9e69312c0ed8c9d9fb0c49f27e0
Mathieu Lemay <acidrain1@gmail.com>
2023-10-08 12:12:04 -0400

    Stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{Read, Write};
+use std::io::{Read, Result, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
+use std::str;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -23,17 +24,48 @@ fn handle(stream: &mut TcpStream) {
     let addr = stream.peer_addr().expect("Unable to get peer_addr");
     println!("accepted new connection from {:?}", addr);
 
-    let mut buf: Vec<u8> = Vec::new();
+    let req = match get_request(stream) {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("Error reading stream: {:?}", e);
+            return;
+        }
+    };
 
-    if let Err(e) = stream.read(&mut buf) {
-        eprintln!("Error reading stream: {:?}", e);
-        return;
-    }
-    println!("Successfully read {} bytes from stream", buf.len());
+    let resp = if req.path == "/" {
+        "HTTP/1.1 200 OK\r\n\r\n"
+    } else {
+        "HTTP/1.1 404 Not Found\r\n\r\n"
+    };
 
-    let resp = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
-    match stream.write(resp) {
+    match stream.write(resp.as_bytes()) {
         Ok(n) => println!("Successfully wrote {} bytes to stream", n),
         Err(e) => eprintln!("Error writing to stream: {:?}", e),
     }
+}
+
+struct Request {
+    method: String,
+    path: String,
+}
+
+fn get_request(stream: &mut TcpStream) -> Result<Request> {
+    let mut buf = [0; 2048];
+
+    let n = stream.read(&mut buf)?;
+    println!("Successfully read {} bytes from stream", n);
+
+    let start_line = str::from_utf8(&buf)
+        .expect("Invalid http request")
+        .split("\r\n")
+        .next()
+        .expect("No start line found");
+
+    println!("Start line: {}", start_line);
+    let parts: Vec<&str> = start_line.split(" ").collect();
+
+    return Ok(Request {
+        method: String::from(parts[0]),
+        path: String::from(parts[1]),
+    });
 }
